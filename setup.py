d48c877344f3d54dde6bb1df31cb6eba6886e91e
Júlio Trevisan <juliotrevisan@gmail.com>
2024-04-04 08:29:31 -0300

    new version ...

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     name=PACKAGE_NAME,
     packages=find_packages(),
     include_package_data=True,
-    version='23.7.26.0',
+    version='24.4.4.0',
     license='GNU GPLv3',
     platforms='any',
     description='Python interface to the PFANT spectral synthesis code, with a varied set of extra tools',
